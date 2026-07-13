8de5dfbdeb3d4377645265a01113bd49c12800e8
Sanaaa77 <mousavi.sanaa@gmail.com>
2026-07-13 17:32:32 +0330

    Delete features directory

--- features/decision/DecisionCenter.tsx
+++
@@ -1,3 +0,0 @@
-"use client";
-import React from "react";
-export const DecisionCenter = () => <div className="p-10 glass rounded-card">Decision Hub</div>;
--- features/universities/services/university-detail.service.ts
+++
@@ -1,1 +0,0 @@
-export const UniversityDetailService = { async getFullProfile() { return null; } };
