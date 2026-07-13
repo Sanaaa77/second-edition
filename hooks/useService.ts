02630b8362f2f19f06b3bfbe27c7153826f37524
Sanaaa77 <mousavi.sanaa@gmail.com>
2026-07-13 17:32:56 +0330

    Delete hooks directory

--- hooks/useService.ts
+++
@@ -1,6 +0,0 @@
-import { container } from "@/lib/core/di/Container";
-import { TOKENS } from "@/lib/core/di/registry";
-
-export function useService<T>(token: keyof typeof TOKENS): T {
-  return container.resolve<T>(TOKENS[token]);
-}
